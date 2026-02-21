6fe6684cbe6d28cacdecd0727d6ea59a49c1953e
pozetiv4ik <stepan.ewqazsx@gmail.com>
2026-02-22 02:12:52 +0300

    feat: create sort list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TaskContext from "./context/taskContext";
 import FilterContext from "./context/filterContext";
 import ToDoList from "./ToDoList";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
   const [filter, setFilter] = useState("all");
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks, filter]);
   return (
     <TaskContext.Provider value={{ tasks, setTasks }}>
       <FilterContext.Provider value={{ filter, setFilter }}>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,29 +2,30 @@ import { useContext, useMemo } from "react";
 import TaskContext from "../context/taskContext";
 import Card from "./Card";
 import FilterContext from "../context/filterContext";
+import { useEffect } from "react";
 
 export default function List() {
   const { tasks } = useContext(TaskContext);
   const { filter } = useContext(FilterContext);
 
   const filteredList = useMemo(() => {
-    
+    const filtred = [...tasks].sort((a, b) => {
+      return a.isDone - b.isDone;
+    });
     switch (filter) {
       case false:
-        return tasks.filter((item) => !item.isDone);
+        return filtred.filter((item) => !item.isDone);
       case true:
-        return tasks.filter((item) => item.isDone);
+        return filtred.filter((item) => item.isDone);
       default:
-        return tasks;
+        return filtred;
     }
   }, [tasks, filter]);
 
-  
-  
   return (
     <div className="block">
       {filteredList.map((item) => (
-        <Card item={item} />
+        <Card key={item.id} item={item} />
       ))}
     </div>
   );
